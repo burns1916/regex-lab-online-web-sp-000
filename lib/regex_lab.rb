2654ed81c969202978904c09382357a98cb67f68
Ryan K Burns <ryankburns@gmail.com>
2020-09-15 16:05:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+if word.scan(/^[aeiouAEIOU]/)
+  true
+else
+  false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
