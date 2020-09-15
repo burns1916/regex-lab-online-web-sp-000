8cd93a31628d84ea2ef1ff8166335be8505da5fa
Ryan K Burns <ryankburns@gmail.com>
2020-09-15 16:39:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan(/^[A-Z].*[?.!]$/) ? true : false
+if text.scan(/^[A-Z].*[?.!]$/)
+  true 
+else
+false
+end
 end
 
 def valid_phone_number?(phone)
