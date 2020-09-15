469fff3ed73ac7abe2e220596f834effb8008f96
Ryan K Burns <ryankburns@gmail.com>
2020-09-15 16:12:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone = valid_numbers
+  valid_numbers = phone
   valid numbers.all? { |number|
 number.scan(/\d{3}-\d{3}-\d{4}/)}
 end
